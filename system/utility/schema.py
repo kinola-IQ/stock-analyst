ee686c5637671623a768f6f0f4118d66a5a7b456
akinola <akdunmininu@gmail.com>
2026-04-11 23:24:07 +0100

    updated files

--- system/utility/schema.py
+++ system/utility/schema.py
@@ -1,18 +1,28 @@
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
 from fastapi.exceptions import RequestValidationError
 
 class UserInputSchema(BaseModel):
-    ticker: str
+    """enforces input requirements for the /agent endpoint"""
+    ticker: str = Field(..., min_length= 3, max_length= 6)
 
-    # setting up default validation
-    @model_validator(mode='after') # tells to run right after the default validations
+    # Standardizing inputted tickers
+    @model_validator(mode='before')
+    def normalize_ticker(cls, values):
+        # Ensure ticker is uppercase before other validations
+        if "ticker" in values and isinstance(values["ticker"], str):
+            values["ticker"] = values["ticker"].upper()
+        return values
+
+    # preventing entry of wrong data type into ticker field
+    @model_validator(mode='after')  # tells to run post-default validations
     def validate_after(self):
-        if not isinstance(self.ticker, str):
+        """validate user input after default validation"""
+        if not isinstance(self.ticker, str) or not self.ticker.isalpha():
             raise RequestValidationError(
-                'must be alphabets'
+                'inputted tickers must be alphabetical only'
             )
         return self
 
-
 class AgentOutputSchema(BaseModel):
+    """schema for agent output"""
     final_summary: str
