e5bcac3768cff7dbd998e1fa0431597e16cb7747
kinola-IQ <akdunmininu@gmail.com>
2026-07-09 09:10:38 +0100

    recovered logging function

--- system/utility/logger.py
+++ system/utility/logger.py
@@ -1,10 +1,19 @@
 """module to log system events"""
 import logging
 import time
+from logging.handlers import RotatingFileHandler
+
+# Create a rotating file handler
+handler = RotatingFileHandler(
+    "app.log",        # log file name
+    maxBytes=100000,    # rotate after ~100KB
+    backupCount=5     # keep 5 old log files
+)
 
 logging.basicConfig(
-    level=logging.INFO,  # log all levels INFO and above
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.DEBUG,  # log all levels DEBUG and above
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[handler]
 )
 
 # instantiating logger
