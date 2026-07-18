3c3944c7530fb5082ee844033538589b80c24d7c
kinola-IQ <akdunmininu@gmail.com>
2026-07-18 20:26:01 +0100

    add Sphinx documentation setup with Makefile, batch script, and configuration files

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Stock Analyst: FastAPI stock research service'
+copyright = '2026, Omolayo-akinola Dumininuoluwa(kinola-iq)'
+author = 'Omolayo-akinola Dumininuoluwa(kinola-iq)'
+release = '7-18-2026'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Pointing to source code -------------------------------------------------
+import os
+import sys
+sys.path.insert(1, os.path.abspath("..")) 
